a377f1cfaab002b693ab79bbeb3e9449d9011fb2
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-17 00:34:50 +0530

    fetched data

--- user-management/src/Components/List.jsx
+++ user-management/src/Components/List.jsx
@@ -1,6 +1,30 @@
-// Todo.js
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
+import axios from "axios";
+
 const List = () => {
+    const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
+    const [edit, setEdit] = useState(null);
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [department, setDepartment] = useState("");
+    const [showPopup, setshowPopup] = useState(false);
+
+    const getRequest = async () => {
+        try {
+          const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+          setData(data);
+        //   console.log(data)
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      useEffect(() => {
+        getRequest();
+      }, []);
+
   return (
     <>
       <div className='container'>
@@ -26,6 +50,23 @@ const List = () => {
             </tr>
           </thead>
           <tbody>
+          {data?.slice((page - 1) * 10, (page - 1) * 10 + 10).map(function (ele, index) {
+              return <tr key={index}>
+                <td>{ele.id}</td>
+                <td>{edit === index ? <input onChange={(e) => setName(e.target.value)} type="text" defaultValue={ele.name.split(' ')[0]} /> : `${ele.name.split(' ')[0]}`}</td>
+                <td>{edit === index ? <input onChange={(e) => setLastName(e.target.value)} type="text" defaultValue={ele.name.split(' ')[1]} /> : `${ele.name.split(' ')[1]}`}</td>
+                <td>{edit === index ? <input onChange={(e) => setEmail(e.target.value)} type="email" defaultValue={ele.email} /> : `${ele.email}`}</td>
+                <td>{edit === index ? <input onChange={(e) => setDepartment(e.target.value)} type="text" defaultValue={ele.company.name} /> : `${ele.company.name}`}</td>
+                <td className="actions-btn">
+                  {edit === index ? (
+                    <button>Update <span></span></button>
+                  ) : (
+                    <button >Edit <span></span></button>
+                  )}
+                  <button>Delete<span></span></button>
+                  </td>
+              </tr>
+            })}
           </tbody>
         </table>
       </div>
